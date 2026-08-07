4a4c609b13ec503cd09d2a769b8fe67ada9eb109
Aman Verma <amankh8127@gmail.com>
2026-08-07 12:34:17 +0530

    23.function.cpp

--- 23.function.cpp
+++ 23.function.cpp
@@ -4,9 +4,18 @@ void printHello(){
     cout <<"Hello\n";
 
 }
+int max(int a, int b){
+    if(a>b){
+         cout << "max element a: " ;
+        return a;
+    }
+    else{
+        cout << "max element b: " ;
+        return b;
+    }
+
+}
 int main(){
-    printHello();
-    printHello();
-    
+    cout << max(10, 20) << endl;
     return 0;
 }
