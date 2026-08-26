5091d3224ee14d2058f8892d5701cbcc3d09e557
Aman Verma <amankh8127@gmail.com>
2026-08-27 00:32:40 +0530

    Added 31.scope

---
+++ 31.scope.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << sizeof(int) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(bool) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(long long) << endl;
+    cout << sizeof(short) << endl;
+    cout << sizeof(long double) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(signed int) << endl;
+    cout << sizeof(unsigned int) << endl;
+    
+    return 0;
+}
