b0fb48a48a24eacf689ada73597db85c2e4e8e96
Aman Verma <amankh8127@gmail.com>
2026-08-26 23:54:08 +0530

    Added 30.BitwiseOperation

---
+++ 30.BitwiseOperation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4, b = 8;
+    // bitwise AND, OR, XOR operations
+    cout << "(a&b) = " << (a&b) << endl;
+    cout << "(a|b) = " << (a|b) << endl;
+    cout << "(a^b) = " << (a^b) << endl;
+    // bitwise left shift operation
+    cout << "(4<<1) = " << (4<<1) << endl;
+    // bitwise right shift operation
+    cout << "(4>>1) = " << (4>>1) << endl;
+    return 0;
+}
