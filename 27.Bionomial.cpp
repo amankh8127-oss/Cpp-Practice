503f2fb8fca3b18e1feaec7e27babcf9f712a692
Aman Verma <amankh8127@gmail.com>
2026-08-26 17:42:27 +0530

    Added 27.Bionomial

---
+++ 27.Bionomial.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+    for(int i=1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+int nCr(int n, int r){
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_n_r = factorial(n-r);
+    
+    return fact_n/(fact_r*fact_n_r);
+}
+int main(){
+    int n, r;
+    cout << "enter the value of n: ";
+    cin >> n;
+    cout << "enter the value of r: ";
+    cin >> r;
+    cout << "nCr = " << n << "C" << r << " = " << nCr(n, r) << endl;
+    return 0;
+}
