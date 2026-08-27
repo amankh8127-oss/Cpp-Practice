a6af01cf64a33fd999b12e7d367a420e0f2d98ef
Aman Verma <amankh8127@gmail.com>
2026-08-27 17:00:10 +0530

    Added 32.ArrayInDSA

---
+++ 32.ArrayInDSA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter the array elements :";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    cout << " the array elements are: ";
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
